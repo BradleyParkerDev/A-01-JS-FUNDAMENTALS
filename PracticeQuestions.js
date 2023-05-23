cedb7d59383ee2c957b07703dbcc11a55f489478
Bradley Parker <Bradleyscottparker@outlook.com>
2023-05-23 09:56:34 -0500

    updated

--- PracticeQuestions.js
+++ PracticeQuestions.js
@@ -314,3 +314,13 @@ fetchData().then((value)=>{
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 16. Call fetchData (which returns a promise) and use the async/await method to log the value the promise resolves with to the javascript console.
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const fetchDataAsync = async () => {
+  try {
+    const results =  await fetchData()
+    console.log(results);
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+fetchDataAsync()
